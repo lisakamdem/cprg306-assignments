3e20904f85aef99fa0141da855fc7aac6a2d802d
lisakamdem <120063552+lisakamdem@users.noreply.github.com>
2023-09-29 20:41:16 -0600

    week 4 assignments

--- app/page.js
+++ app/page.js
@@ -122,6 +122,7 @@ export default function Page (){
     <StudentInfo />
     <p><Link href="/week2">Week 2</Link> </p>
     <p><Link href="/week3">Week 3</Link></p>
+    <p><Link href="/week4">Week 4</Link></p>
   </main>
   );
 } 
---
+++ app/week4/page.js
@@ -0,0 +1,10 @@
+import NewItem from "./new-item";
+
+export default function Page() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold m-2 text-purple-500 text-center">Add Item</h1>
+      <NewItem/>
+    </main>
+  );
+}
