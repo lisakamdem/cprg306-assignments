aab437b2461899cf14c4aa592b2fc15f6ff90ff1
lisakamdem <120063552+lisakamdem@users.noreply.github.com>
2023-09-29 20:47:37 -0600

    week 4 assignment

--- app/page.js
+++ app/page.js
@@ -118,7 +118,7 @@ import StudentInfo  from "./student-info";
 export default function Page (){
   return (
   <main> 
-    <h1> CPRG 306: Web Development 2 - Assignments</h1>
+    <h1> CPRG-306: Web Development 2 - Assignments</h1>
     <StudentInfo />
     <p><Link href="/week2">Week 2</Link> </p>
     <p><Link href="/week3">Week 3</Link></p>
--- app/student-info.js
+++ app/student-info.js
@@ -4,7 +4,7 @@ export default function StudentInfo (){
     return (
         <>
             <h1>Your name: Lisa kamdem</h1>
-            <p>Course section: CPRG 306 A</p>
+            <p>Course section: CPRG-306-A</p>
             <Link href = "https://github.com/lisakamdem">My GitHub</Link>  
         </>
         
